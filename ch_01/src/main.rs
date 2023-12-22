3d8fe29e2d0fab41024bd7cbbf4a64d48b3b0d30
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-22 21:26:33 +0200

    🎉 feat(main.rs): Add showcases for safety, concurrency, performance, and zero-overhead principles in Rust program

--- ch_01/src/main.rs
+++ ch_01/src/main.rs
@@ -1,8 +1,55 @@
+// A Rust program showcasing safety, concurrency, performance, and zero-overhead principles
+
+// Safety: Demonstrating memory safety with ownership and borrowing
+fn demonstrate_safety() {
+    let mut data = vec![1, 2, 3];
+    let reference_to_data = &mut data;
+
+    reference_to_data.push(4);
+    println!("Demonstrating memory safety: {:?}", data);
+}
+
+// Concurrency: Using threads for concurrent execution
+fn demonstrate_concurrency() {
+    use std::thread;
+
+    let handle = thread::spawn(|| {
+        println!("Concurrent execution using threads");
+    });
+
+    handle.join().unwrap();
+}
+
+// Performance: Leveraging Rust's optimization and predictable performance
+fn demonstrate_performance() {
+    let _arr = [1, 2, 3];
+
+    // Demonstrating array bounds checking for predictable performance
+    // Uncommenting the line below would cause a run-time panic (index out of bounds)
+    // let value = arr[5];
+}
+
+// Zero-Overhead Principle: Emphasizing efficient abstractions without runtime overhead
+fn zero_overhead_principle() {
+    // Rust provides high-level abstractions without performance penalties
+    let maybe_number: Option<i32> = Some(42);
+
+    if let Some(value) = maybe_number {
+        println!(
+            "Zero-overhead principle: Handling Option type safely - Value: {}",
+            value
+        );
+    } else {
+        println!("Zero-overhead principle: Handling Option type safely - No value!");
+    }
+}
+
 fn main() {
-    let mut a = [0u64; 1]; // Declaring an array 'a' of length 1
+    println!("Rust Program Summary:");
 
-    // Attempting to assign a value to an out-of-bounds index would result in a compile-time error
-    // a[3] = 0x7ffff7b36ceb;
-
-    // Uncommenting the line above would cause a compile-time error due to out-of-bounds access
+    // Demonstrating key aspects of Rust
+    demonstrate_safety();
+    demonstrate_concurrency();
+    demonstrate_performance();
+    zero_overhead_principle();
 }
