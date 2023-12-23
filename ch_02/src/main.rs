63916e7e835ee2ed191f50c1b20011d5b75bc912
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-23 06:19:10 +0200

    🔧 refactor(Readme.md): Update GCD algorithm code and explanation 🐛 fix(main.rs): Update main function to include gcd function calls

--- ch_02/src/main.rs
+++ ch_02/src/main.rs
@@ -1,3 +1,17 @@
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            (m, n) = (n, m)
+        }
+        m %= n;
+    }
+    n
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(gcd(14, 21), 7);
+    assert_eq!(gcd(49, 14), 7);
+    assert_eq!(gcd(1, 100), 1);
+    assert_eq!(gcd(6, 9), 3);
 }
