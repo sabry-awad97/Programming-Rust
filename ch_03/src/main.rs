efc83da65f397feabbd542c7a96f069044f9a1a8
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-23 17:37:21 +0200

    ✨ feat(main.rs): Add arithmetic operations and error handling for u8 variables

--- ch_03/src/main.rs
+++ ch_03/src/main.rs
@@ -1,9 +1,64 @@
 fn main() {
-    let a: u8 = 255; // Maximum value for u8
-    let b: u8 = 1;
+    // Addition
+    let x = 1u8;
+    let y = 2u8;
 
-    // Overflow occurs when adding 'a' and 'b' as their sum exceeds the maximum value for u8
-    let result = a.wrapping_add(b); // Using wrapping_add to demonstrate overflow behavior
+    let checked_result = x.checked_add(y);
+    let wrapping_result = x.wrapping_add(y);
+    let saturating_result = x.saturating_add(y);
+    let (overflowing_result, overflowed) = x.overflowing_add(y);
 
-    println!("Overflow Example: {}", result);
+    // Subtraction
+    let x = 1u8;
+    let y = 2u8;
+
+    let checked_result = x.checked_sub(y);
+    let wrapping_result = x.wrapping_sub(y);
+    let saturating_result = x.saturating_sub(y);
+    let (overflowing_result, overflowed) = x.overflowing_sub(y);
+
+    // Multiplication
+    let x = 2u8;
+    let y = 3u8;
+
+    let checked_result = x.checked_mul(y);
+    let wrapping_result = x.wrapping_mul(y);
+    let saturating_result = x.saturating_mul(y);
+    let (overflowing_result, overflowed) = x.overflowing_mul(y);
+
+    assert_eq!(checked_result, Some(6));
+    assert_eq!(wrapping_result, 6);
+    assert_eq!(saturating_result, 6);
+    assert_eq!(overflowing_result, 6);
+    assert!(!overflowed);
+
+    // Division
+    let x = 10u8;
+    let y = 3u8;
+
+    let checked_result = x.checked_div(y);
+    let wrapping_result = x.wrapping_div(y);
+
+    // Remainder
+    let x = 10u8;
+    let y = 3u8;
+
+    let checked_result = x.checked_rem(y);
+    let wrapping_result = x.wrapping_rem(y);
+
+    // Left shift
+    let x = 1u8;
+    let y = 2u8;
+
+    let checked_result = x.checked_shl(y as u32);
+    let wrapping_result = x.wrapping_shl(y as u32);
+    let (overflowing_result, overflowed) = x.overflowing_shl(y as u32);
+
+    // Right shift
+    let x = 1u8;
+    let y = 2u8;
+
+    let checked_result = x.checked_shr(y as u32);
+    let wrapping_result = x.wrapping_shr(y as u32);
+    let (overflowing_result, overflowed) = x.overflowing_shr(y as u32);
 }
