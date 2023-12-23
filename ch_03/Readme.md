64e950abf81d168ec271e724f97344b1c3877b53
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-23 19:08:55 +0200

    🔄 refactor(Readme.md, main.rs): Refactor code for converting characters 🐛 fix(Readme.md, main.rs): Fix typo in escape_default method

--- ch_03/Readme.md
+++ ch_03/Readme.md
@@ -795,3 +795,33 @@ fn main() {
     assert_eq!(h, 'H');
 }
 ```
+
+1. The `char` type has a number of methods for converting its value to and from various representations:
+
+   ```rs
+   fn main() {
+       let c: char = 'a';
+       let s1: String = c.escape_unicode().collect();
+       assert_eq!(s1, "\\u{61}");
+
+       let s2: String = c.escape_debug().collect();
+       assert_eq!(s2, "a");
+
+       let s3: String = c.escape_default().collect();
+       assert_eq!(s3, "a");
+
+       let d: char = '\u{1f600}';
+       let s4: String = d.escape_unicode().collect();
+       assert_eq!(s4, "\\u{1f600}");
+
+       let s5: String = d.escape_debug().collect();
+       assert_eq!(s5, "😀");
+
+       let s6: String = d.escape_default().collect();
+       assert_eq!(s6, "\\u{1f600}");
+
+       let s7: String = "a".to_string();
+       let f: char = s7.parse().unwrap();
+       assert_eq!(f, 'a');
+   }
+   ```
--- ch_03/src/main.rs
+++ ch_03/src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
     let c: char = 'a';
-    let d: char = c.to_uppercase().next().unwrap();
-    assert_eq!(d, 'A');
+    let s1: String = c.escape_unicode().collect();
+    assert_eq!(s1, "\\u{61}");
 
-    let e: char = 'A';
-    let f: char = e.to_lowercase().next().unwrap();
+    let s2: String = c.escape_debug().collect();
+    assert_eq!(s2, "a");
+
+    let s3: String = c.escape_default().collect();
+    assert_eq!(s3, "a");
+
+    let d: char = '\u{1f600}';
+    let s4: String = d.escape_unicode().collect();
+    assert_eq!(s4, "\\u{1f600}");
+
+    let s5: String = d.escape_debug().collect();
+    assert_eq!(s5, "😀");
+
+    let s6: String = d.escape_default().collect();
+    assert_eq!(s6, "\\u{1f600}");
+
+    let s7: String = "a".to_string();
+    let f: char = s7.parse().unwrap();
     assert_eq!(f, 'a');
-
-    let g: char = 'h';
-    let h: char = g.to_ascii_uppercase();
-    assert_eq!(h, 'H');
 }
