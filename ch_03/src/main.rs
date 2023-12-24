73bea738256c40273115384812a730344dc6284b
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-24 16:46:45 +0200

    🎉 feat(Readme.md): Add examples of using arrays in Rust 🐛 fix(main.rs): Correct code to iterate over array elements using an iterator

--- ch_03/src/main.rs
+++ ch_03/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let mut num = 42;
-
-    // Creating unsafe mutable raw pointer
-    let unsafe_ptr: *mut i32 = &mut num;
-
-    // Dereferencing the raw pointer within unsafe block
-    unsafe {
-        *unsafe_ptr = 10;
-    }
-
-    println!("Modified value: {}", num);
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut iter = xs.iter();
+    assert_eq!(iter.next(), Some(&1)); // iter is now at the second element
+    assert_eq!(iter.next(), Some(&2)); // iter is now at the third element
+    assert_eq!(iter.next(), Some(&3)); // iter is now at the fourth element
+    assert_eq!(iter.next(), Some(&4)); // iter is now at the fifth element
+    assert_eq!(iter.next(), Some(&5)); // iter is at the end
+    assert_eq!(iter.next(), None); // iter is at the end
 }
