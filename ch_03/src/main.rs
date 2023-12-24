9958da4e41bb04f73eb74f9825e99e23b447c5ee
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-24 20:33:02 +0200

    🎉 feat(main.rs): Implement deduplication of vector based on condition

--- ch_03/src/main.rs
+++ ch_03/src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let xs: [i32; 5] = [1, 2, 3, 4, 5];
-    let mut iter = xs.iter();
-    assert_eq!(iter.next(), Some(&1)); // iter is now at the second element
-    assert_eq!(iter.next(), Some(&2)); // iter is now at the third element
-    assert_eq!(iter.next(), Some(&3)); // iter is now at the fourth element
-    assert_eq!(iter.next(), Some(&4)); // iter is now at the fifth element
-    assert_eq!(iter.next(), Some(&5)); // iter is at the end
-    assert_eq!(iter.next(), None); // iter is at the end
+    let mut xs = vec![1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+    xs.dedup_by(|&mut a, &mut b| a % 2 == b % 2); // xs is now [1, 2, 3, 4]
 }
