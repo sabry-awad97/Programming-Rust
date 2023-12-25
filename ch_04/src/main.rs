9929415d0fe954719ca09af4cc2bd344ef631951
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-25 22:34:13 +0200

    🐛 fix(main.rs): Resolve data race in multi-threaded application
    ✨ feat(main.rs): Implement shared data synchronization using Arc and Mutex

--- ch_04/src/main.rs
+++ ch_04/src/main.rs
@@ -1,24 +1,33 @@
-use std::rc::Rc;
-
-struct Node {
-    value: i32,
-    next: Option<Rc<Node>>,
-}
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let mut a = Rc::new(Node {
-        value: 1,
-        next: None,
-    });
-    let b = Rc::new(Node {
-        value: 2,
-        next: Some(a.clone()),
-    });
-    let c = Rc::new(Node {
-        value: 3,
-        next: Some(b.clone()),
+    // Creating shared data wrapped in an Arc and a Mutex
+    let shared_data = Arc::new(Mutex::new(0));
+
+    // Cloning Arc to create multiple references for different threads
+    let shared_data_clone1 = Arc::clone(&shared_data);
+    let shared_data_clone2 = Arc::clone(&shared_data);
+
+    // Spawn threads to concurrently modify the shared data
+    let thread1 = std::thread::spawn(move || {
+        let mut data = shared_data_clone1.lock().unwrap();
+        *data += 1; // Modifying the shared data
     });
 
-    // Create a reference cycle by setting a's next value to c
-    a.next = Some(c.clone()); // error: cannot assign to data in an `Rc`
+    let thread2 = std::thread::spawn(move || {
+        let mut data = shared_data_clone2.lock().unwrap();
+        *data += 2; // Modifying the shared data
+    });
+
+    // Waiting for threads to complete execution
+    thread1.join().unwrap();
+    thread2.join().unwrap();
+
+    // Accessing the modified shared data
+    let final_data = shared_data.lock().unwrap();
+    println!("Final value: {}", *final_data);
+
+    // Get the current reference count
+    let count = Arc::strong_count(&shared_data);
+    println!("Reference count of shared_data: {}", count);
 }
