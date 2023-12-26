e4f9f7d961a0b46851bdc4175037a39412514a97
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-26 23:48:35 +0200

    ✨ feat(Readme.md): Add explanation of passing references to functions 🐛 fix(main.rs): Fix function name and remove unnecessary code

--- ch_05/src/main.rs
+++ ch_05/src/main.rs
@@ -1,15 +1,9 @@
-static mut STASH: &i32 = &10;
-
-fn foo(x: &'static i32) {
-    unsafe {
-        STASH = x;
-    }
+fn f(p: &'static i32) {
+    println!("{}", p);
 }
 
-static WORTH_POINTING_AT: i32 = 1000;
-
 fn main() {
-    let y = 5;
-    foo(&y); // error: y has a shorter lifetime than STASH
-    foo(&WORTH_POINTING_AT); // okay, WORTH_POINTING_AT is a static
+    let x = 10;
+    let y: &'static i32 = unsafe { std::mem::transmute(&x) };
+    f(y);
 }
