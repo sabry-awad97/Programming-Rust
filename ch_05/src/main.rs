ce20dc7e43c2556fcfc5035fc8f63c14d042dc03
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-27 00:49:59 +0200

    🎉 feat(ch_05): Implement StringTable with find_by_prefix method
    ✨ feat(ch_05): Add example usage of StringTable with find_by_prefix method

--- ch_05/src/main.rs
+++ ch_05/src/main.rs
@@ -1,5 +1,36 @@
-fn foo<'a>(x: &'a i32) -> (&i32, &i32) {
-    (x, x)
+struct StringTable {
+    elements: Vec<String>,
 }
 
-fn main() {}
+impl StringTable {
+    fn find_by_prefix(&self, prefix: &str) -> Option<&String> {
+        for i in 0..self.elements.len() {
+            if self.elements[i].starts_with(prefix) {
+                return Some(&self.elements[i]);
+            }
+        }
+        None
+    }
+}
+
+fn main() {
+    let mut table = StringTable {
+        elements: Vec::new(), // Create an empty StringTable
+    };
+    table.elements.push("apple".to_string());
+    table.elements.push("banana".to_string());
+    table.elements.push("orange".to_string());
+
+    // Search for strings starting with a specific prefix
+    let prefix = "app";
+    match table.find_by_prefix(prefix) {
+        Some(found_string) => {
+            println!("String found: {}", found_string);
+            // Use the found_string reference here...
+        }
+        None => {
+            println!("No string found with the prefix: {}", prefix);
+            // Handle the case where no matching string is found...
+        }
+    }
+}
