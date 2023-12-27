1ee369b6af78bd6a4f3e05c22f0d55c75c17307e
Sabry Awad <dr.sabry1997@gmail.com>
2023-12-27 03:19:18 +0200

    🐛 fix(main.rs): Add backtrace dependency and handle recursive_function errors

--- ch_07/src/main.rs
+++ ch_07/src/main.rs
@@ -1,44 +1,22 @@
 use std::error::Error;
-use std::fs::File;
-use std::io;
-use std::path::PathBuf;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let file_path = PathBuf::from("nonexistent.txt");
-    let file = File::open(&file_path).map_err(|e| MyError::from((e, file_path.clone())))?;
-    Ok(())
-}
-
-#[derive(Debug)]
-struct MyError {
-    source: Option<Box<dyn Error>>,
-    path: Option<PathBuf>,
-}
-
-impl MyError {
-    fn from((error, path): (io::Error, PathBuf)) -> MyError {
-        MyError {
-            source: Some(Box::new(error)),
-            path: Some(path),
-        }
+use backtrace::Backtrace;
+fn recursive_function(n: i32) -> Result<(), Box<dyn Error>> {
+    if n == 0 {
+        Err("error message".into())
+    } else {
+        recursive_function(n - 1)
     }
 }
 
-impl Error for MyError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        self.source.as_ref().map(|e| e.as_ref())
-    }
-}
-
-impl std::fmt::Display for MyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Error opening file")?;
-        if let Some(path) = &self.path {
-            write!(f, ": {:?}", path)?;
+fn main() {
+    let bt = Backtrace::new();
+    let result = recursive_function(10);
+    match result {
+        Ok(_) => println!("Success!"),
+        Err(e) => {
+            println!("Error: {}", e);
+            println!("Backtrace: {:?}", bt);
         }
-        if let Some(source) = &self.source {
-            write!(f, ": {}", source)?;
-        }
-        Ok(())
     }
 }
